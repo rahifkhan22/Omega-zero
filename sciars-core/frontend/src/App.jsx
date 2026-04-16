7c34fde30dcf24178a5b508eb23f582dacbf4c2c
rahif khan <rahifkhan59@gmail.com>
2026-04-16 15:48:52 +0530

    made changes in frontend

--- sciars-core/frontend/src/App.jsx
+++ sciars-core/frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ReportIssue from "./pages/ReportIssue";
+import DashboardUser from "./pages/DashboardUser";
+import DashboardSupervisor from "./pages/DashboardSupervisor";
+import DashboardAdmin from "./pages/DashboardAdmin";
+
 function App() {
   return (
-    <div>
-      <h1>Hello SCIARS</h1>
-      <p>App is working</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/report" element={<ReportIssue />} />
+        <Route path="/user" element={<DashboardUser />} />
+        <Route path="/supervisor" element={<DashboardSupervisor />} />
+        <Route path="/admin" element={<DashboardAdmin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
